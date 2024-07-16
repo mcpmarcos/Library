056beccd0a5875c0a12b89f957c2ae62c5785ea7
Marcos Pacheco <mcp_marcos@hotmail.com>
2024-07-16 18:46:39 -0300

    Booklist return test done.

--- src/test/java/com/the_library/controller/BookControllerTest.java
+++ src/test/java/com/the_library/controller/BookControllerTest.java
@@ -1,65 +1,51 @@
 package com.the_library.controller;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.the_library.controllers.BookController;
+import com.the_library.DTO.BookDTO;
 import com.the_library.models.Book;
-import com.the_library.repositories.BookRepository;
+import com.the_library.services.BookService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-//@WebMvcTest(BookController.class)
 public class BookControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
     
     @MockBean
-    BookRepository repository;
+    BookService service;
     
-    @Test
-    void findAllBooksTest() throws Exception{
-        Book book = new Book();
-        book.setAuthor("Marcos Pacheco");
-        Mockito.when(repository.findAll()).thenReturn(List.of(book));
-        this.mockMvc.perform(get("/books"))
-        .andDo(print())
-        .andExpect(status().isOk());
-        
-        
-        /*
-        @Test
-        void findAllBooks() throws Exception{
-            Book book = new Book();
-            book.setAuthor("Marcos Pacheco");
-            Mockito.when(repository.findAll()).thenReturn(List.of(book));
-            this.mockMvc.perform(get("/books"))
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(content().json("[{'id':1, tittle:'Book 3',author:'Marcos Pacheco'}]"));
-         
-        */
+    BookDTO bookDTO;
+    
+    List<BookDTO> bookListDTO;
 
-    /*
-    @Test
-    void creatBookTest() throws Exception{
-       this.mockMvc.perform(get("/books").andDo(print()).andExpect);
+    @BeforeEach
+    void setup(){
+        Book book1 = new Book(666,"Tiger Tiger", "José Torres"); 
+        Book book2 = new Book(666,"As tranças do careca", "GBrennon");
+        List<Book> books = new ArrayList<>();
+        books.add(book1); 
+        books.add(book2); 
+        bookListDTO = service.createDTO(books);
     }
-    */
+
+    @Test
+    void returnedBookListTest() throws Exception{
+        Mockito.when(service.findAllBooks()).thenReturn(bookListDTO);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/books").contentType("APPLICATION_JSON")).andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
